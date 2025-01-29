dd6e57a664d3ddf2a9094afbe638b614db6c1ad7
mariajose132 <metrixprograming@gmail.com>
2025-01-29 13:01:39 -0500

    Agregar clase.py

--- clase.py
+++ clase.py
@@ -1,9 +1,1 @@
-class Persona:
-    def __init__(self, nombre):
-        self.nombre = nombre
-
-    def saludar(self):
-        return f"Hola, soy {self.nombre}"
-
-persona = Persona("María José")
-print(persona.saludar())
+print("Hola, GitHub!")
